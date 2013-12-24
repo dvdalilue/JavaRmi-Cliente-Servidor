518e6f9d1c7fa7542fd051267a9e890908a20f11
David <dvdalilue@gmail.com>
2013-12-24 06:55:36 -0430

    Eliminacion de la funcion remota de suma & static void main

--- RmiServer.java
+++ RmiServer.java
@@ -1,9 +1,19 @@
 import java.rmi.Remote;
 import java.rmi.RemoteException;
-
+/**
+ * @author      David Lilue <dvdalilue@gmail.com>
+ * @version     1.0          
+ * @since       2013-12-23
+ */
 public interface RmiServer extends Remote {
 
+    /**
+     * Obtiene un mensaje.
+     * <p>
+     * Devuelve um mensaje definido.
+     * <p>
+     *
+     * @return sting del mensaje
+     */
     public String getMessage() throws RemoteException;
-
-    public long add(long a, long b) throws RemoteException;
 }
--- RmiServerImpl.java
+++ RmiServerImpl.java
@@ -1,40 +1,28 @@
-import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
-import java.rmi.registry.*; 
-
-public class RmiServerImpl 
+/**
+ * @author      David Lilue <dvdalilue@gmail.com>
+ * @version     1.0          
+ * @since       2013-12-23
+ */
+public class RmiServerImpl
     extends UnicastRemoteObject 
     implements RmiServer {
-    
+    /**
+     * <p>
+     * Contruye la clase a partir del constructor de la clase
+     * extendida UnicastRemoteObject
+     * <p>
+     */
     public RmiServerImpl() throws RemoteException {
         super(0);
     }
-
+    /**
+     * Devuelve un mensaje.
+     *
+     * @return el string de un mensaje
+     */
     public String getMessage() {
         return "Hello!!";
-    }
-
-    public long add(long a, long b) {
-        return a + b;
-    }
-
-    public static void main(String args[]) throws Exception {
-        System.out.println("RMI server started");
- 
-        try { //special exception handler for registry creation
-            LocateRegistry.createRegistry(1099); 
-            System.out.println("java RMI registry created.");
-        } catch (RemoteException e) {
-            //do nothing, error means registry already exists
-            System.out.println("java RMI registry already exists.");
-        }
- 
-        //Instantiate RmiServer
-        RmiServerImpl obj = new RmiServerImpl();
- 
-        // Bind this object instance to the name "RmiServer"
-        Naming.rebind("//localhost/RmiServerImpl", obj);
-        System.out.println("PeerServer bound in registry");
     }
 }
