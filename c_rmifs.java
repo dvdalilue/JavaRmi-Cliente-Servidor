0b87a6fc69be09eeee163afd52b0168f9a5603df
David <dvdalilue@gmail.com>
2014-01-07 16:45:37 -0430

    Creacion de una seccion para el reconocimiento de comandos

--- c_rmifs.java
+++ c_rmifs.java
@@ -1,6 +1,7 @@
 import java.rmi.Naming;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.io.File;
+import java.util.*;
+import java.io.*;
 /**
  * @author      David Lilue <dvdalilue@gmail.com>
  * @version     1.0          
@@ -17,16 +18,38 @@ public class c_rmifs {
      * cuando se ejecuta el main
      */
     public static void main(String args[]) throws Exception {
-        RmiServer obj = (RmiServer)Naming.lookup("//localhost/RmiServerImpl");
-        System.out.println(obj.getMessage());
-        RmiClient cli = new RmiClient();
-        cli.directory_stream();
-        System.out.println("");
-        for (Path file : cli.stream) {
-            if (!Files.isDirectory(file)) {
-                System.out.println("  " + file.getFileName());
+        try {
+            RmiServer obj = (RmiServer)Naming.lookup("rmi://localhost:1099/RmiService");
+            //RmiServer obj = (RmiServer)Naming.lookup("rmi://serena.ldc.usb.ve:1099/RmiService");
+            RmiClient cli = new RmiClient();
+            //cli.info();
+            //System.out.println(obj.del("delete"));
+            while (true) {
+                System.out.print("Cliente Rmi~$ ");
+                
+                //  open up standard input
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                
+                String cmd = null;
+                
+                //  read the username from the command-line; need to use try/catch with the
+                //  readLine() method
+                try {
+                    cmd = br.readLine();
+                    if (cmd.equals("sal")) {
+                        System.exit(0);
+                    }
+                    else if (cmd.equals("men")) {
+                        System.out.println(obj.getMessage());                        
+                    }
+                } catch (IOException ioe) {
+                    System.out.println("IO error trying to read your name!");
+                    System.exit(1);
+                }
             }
+        } catch (Exception e) {
+            System.out.println("RmiServer exception: " + e);
+            System.exit(1);
         }
-        System.out.println("");
     }
 }
--- s_rmifs.java
+++ s_rmifs.java
@@ -1,6 +1,7 @@
 import java.rmi.RemoteException;
 import java.rmi.Naming;
 import java.rmi.registry.*; 
+import java.io.*;
 /**
  * @author      David Lilue <dvdalilue@gmail.com>
  * @version     1.0          
@@ -17,21 +18,47 @@ public class s_rmifs {
      * cuando se ejecuta el main
      */
     public static void main(String args[]) throws Exception {
-        System.out.println("RMI server started");
+        try {
+            RmiAuthen aut = (RmiAuthen)Naming.lookup("rmi://localhost:1100/RmiAuthentication");
+            System.out.println(aut.getMessage());
+            System.out.println("RMI server started");
  
-        try { //special exception handler for registry creation
-            LocateRegistry.createRegistry(1099); 
-            System.out.println("java RMI registry created.");
-        } catch (RemoteException e) {
-            //do nothing, error means registry already exists
-            System.out.println("java RMI registry already exists.");
+            try { //special exception handler for registry creation
+                LocateRegistry.createRegistry(1099); 
+                System.out.println("java RMI registry created.");
+            } catch (RemoteException e) {
+                //do nothing, error means registry already exists
+                System.out.println("java registry already exists.");
+            }
+ 
+            //Instantiate RmiServer
+            RmiServerImpl obj = new RmiServerImpl();
+ 
+            // Bind this object instance to the name "RmiServer"
+            Naming.rebind("rmi://localhost:1099/RmiService", obj);
+            System.out.println("PeerServer bound in registry");
+            while (true) {
+                System.out.print("Servidor Rmi~$ ");
+                
+                //  open up standard input
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                
+                String cmd = null;
+                
+                //  read the username from the command-line; need to use try/catch with the
+                //  readLine() method
+                try {
+                    cmd = br.readLine();
+                    if (cmd.equals("sal")) {
+                        System.exit(0);
+                    }
+                } catch (IOException ioe) {
+                    System.out.println("IO error trying to read your name!");
+                    System.exit(1);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("RmiServer exception: " + e);
         }
- 
-        //Instantiate RmiServer
-        RmiServerImpl obj = new RmiServerImpl();
- 
-        // Bind this object instance to the name "RmiServer"
-        Naming.rebind("//localhost/RmiServerImpl", obj);
-        System.out.println("PeerServer bound in registry");
     }
 }
