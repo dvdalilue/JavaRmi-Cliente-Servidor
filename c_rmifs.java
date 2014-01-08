4c4e1e9bccab7a8f6d6b48d9b694691c0947ee4f
David <dvdalilue@gmail.com>
2014-01-08 01:06:56 -0430

    Reconocimiento de todos los comandos

--- c_rmifs.java
+++ c_rmifs.java
@@ -19,36 +19,74 @@ public class c_rmifs {
      */
     public static void main(String args[]) throws Exception {
         try {
-            RmiServer obj = (RmiServer)Naming.lookup("rmi://localhost:1099/RmiService");
+            //  open up standard input
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String atr[], name, key, cmd = null;
+            System.out.print("Nombre: ");
+            name = br.readLine();
+            System.out.print("Clave: ");
+            key = br.readLine();
+
+            RmiServer obj = (RmiServer)Naming.lookup("rmi://localhost:1099/RmiService"); 
             //RmiServer obj = (RmiServer)Naming.lookup("rmi://serena.ldc.usb.ve:1099/RmiService");
+            
+            // if (!(obj.authentic(name+":"+key))) {
+            //     System.out.println("***Usuario o clave invalido!!!");
+            //     System.exit(0);
+            // }
             RmiClient cli = new RmiClient();
-            //cli.info();
-            //System.out.println(obj.del("delete"));
             while (true) {
-                System.out.print("Cliente Rmi~$ ");
-                
-                //  open up standard input
-                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-                
-                String cmd = null;
+                System.out.print("\u001B[37m["+name+"@"+"Cliente Rmi:~]$ \u001B[0m");
                 
                 //  read the username from the command-line; need to use try/catch with the
                 //  readLine() method
                 try {
                     cmd = br.readLine();
-                    if (cmd.equals("sal")) {
-                        System.exit(0);
+                    if (!cmd.isEmpty()) {
+                        obj.new_cmd(cmd,name);
+                        atr = cmd.split(" ");
+                        if (cmd.equals("sal")) {
+                            System.exit(0);
+                        }
+                        else if (cmd.equals("info")) {
+                            cli.info();
+                        }
+                        else if (cmd.equals("lls")) {
+                            cli.scan_directory();
+                            cli.to_s();
+                        }
+                        else if (cmd.equals("rls")) {
+                            System.out.println(obj.directory());                        
+                        }
+                        else if (atr[0].equals("sub")) {
+                            try {
+                                System.out.println(atr[1]);
+                            } catch (Exception e) {
+                                System.out.println("Falto identificar el archivo. Ej. sub test.txt");
+                            }
+                        }
+                        else if (atr[0].equals("baj")) {
+                            try {
+                                System.out.println(atr[1]);
+                            } catch (Exception e) {
+                                System.out.println("Falto identificar el archivo. Ej. baj test.txt");
+                            }
+                        }
+                        else if (atr[0].equals("bor")) {
+                            try {
+                                System.out.println(atr[1]);
+                            } catch (Exception e) {
+                                System.out.println("Falto identificar el archivo. Ej. bor test.txt");
+                            }
+                        }
                     }
-                    else if (cmd.equals("men")) {
-                        System.out.println(obj.getMessage());                        
-                    }
-                } catch (IOException ioe) {
-                    System.out.println("IO error trying to read your name!");
+                } catch (Exception e) {
+                    System.out.println("Command exception: " + e);
                     System.exit(1);
                 }
             }
         } catch (Exception e) {
-            System.out.println("RmiServer exception: " + e);
+            System.out.println("RmiClient exception: " + e);
             System.exit(1);
         }
     }
